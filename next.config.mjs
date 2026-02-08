b4cc68a5fbaa0690bfad08bd112e739b4de0adc1
panwarnikita <nikitapanwar593@gmail.com>
2026-02-08 16:34:08 +0530

    Fix memory issue for Render deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,41 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   async headers() {
+//     return [
+//       {
+//         source: "/(.*)",
+//         headers: [
+//           { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
+//           { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
+//         ],
+//       },
+//     ];
+//   },
+//   webpack: (config, { isServer }) => {
+//     if (!isServer) {
+//       config.resolve.fallback = {
+//         ...config.resolve.fallback,
+//         fs: false,
+//         path: false,
+//       };
+//     }
+//     return config;
+//   },
+// };
+
+// export default nextConfig;
+
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // --- INN LINES KO ADD KAREIN (Memory fix ke liye) ---
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  experimental: { webpackBuildWorker: false }, 
+  // --------------------------------------------------
+
   async headers() {
     return [
       {
