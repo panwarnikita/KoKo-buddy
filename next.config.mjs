bbcb6a06f527295edf384b39f6af0ea9bceb8f83
panwarnikita <nikitapanwar593@gmail.com>
2026-02-13 19:16:03 +0530

    Force webpack mode to fix Turbopack conflict

--- next.config.mjs
+++ next.config.mjs
@@ -34,48 +34,10 @@
 
 
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   // Turbopack settings ko rehne dete hain jaisa aapka tha
-//   turbopack: {}, 
-  
-//   async headers() {
-//     return [
-//       {
-//         source: "/(.*)",
-//         headers: [
-//           { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
-//           { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
-//         ],
-//       },
-//     ];
-//   },
-//   webpack: (config, { isServer }) => {
-//     if (!isServer) {
-//       config.resolve.fallback = {
-//         ...config.resolve.fallback,
-//         fs: false,
-//         path: false,
-//         // Piper TTS ko in dono ki bhi zaroorat hoti hai build skip karne ke liye
-//         child_process: false,
-//         crypto: false, 
-//       };
-//     }
-//     return config;
-//   },
-// };
-
-// export default nextConfig;
-
-
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Turbopack ko khali rakhne se Next.js error nahi dega
-  experimental: {
-    turbo: {}
-  },
+  turbopack: {}, 
   async headers() {
     return [
       {
